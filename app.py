ec3bc8dd67faf4a18f305c96276f7ea980357dbd
Sakib_Dalal <sakibdalal73@gmail.com>
2024-11-10 10:08:33 +0530

    update flask app

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+# Flask App Here
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port="80", host="0.0.0.0")
