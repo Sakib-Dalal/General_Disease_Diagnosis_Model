91a5a6352b3584bbb39744b9048818a80605a2a3
Damodhar Zanwar <damodharzanwar5@gmail.com>
2024-11-10 17:36:40 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pandas as pd
 import pickle
+from sklearn.metrics import f1_score
 
 # Flask App
 app = Flask(__name__)
+
+app.secret_key = 'sdakq3wjp29q3jerwo349u1205p4ejfoq8234m0nf380623456245njdkfsgh'
 
 # Load Model
 with open('model_2000.pkl', 'rb') as file:
@@ -12,6 +15,8 @@ with open('model_2000.pkl', 'rb') as file:
 # Home Page
 @app.route('/')
 def home():
+    disease = session.get('disease', None)
+    f1 = session.get('f1', None)
     return render_template("index.html")
 
 # Prediction Route
@@ -27,7 +32,15 @@ def predict():
     predictions = model.predict(input_data)
     disease = predictions[0]
 
-    return f"The predicted disease is: {disease}"
+    true_labels = ['True Disease Label']
+    predicted_labels = predictions[0]
+
+    f1 = f1_score(true_labels, predicted_labels, average='weighted')
+
+    session['disease'] = disease
+    session['f1'] = f1
+    
+    return render_template("index.html", disease = disease, f1 = f1)
 
 if __name__ == "__main__":
     app.run(debug=True, port="80", host="0.0.0.0")
